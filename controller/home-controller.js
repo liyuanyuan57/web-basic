8213fb5f465318225173fbb258ae5cd81f3c4cff
liyuanyuan57 <liyuanyuan0507@outlook.com>
2015-08-13 14:40:59 +0800

    simulate data to test to generate the page of fillIn dynamically

--- controller/home-controller.js
+++ controller/home-controller.js
@@ -3,7 +3,26 @@ function HomeController(){
 }
 HomeController.prototype.getIndex = function (req,res) {
 console.log(req.body);
-res.render('index');
+res.render('index',{
+     "topics":[
+       {
+           "title":"UML 的中文全称是:",
+           "name":"1_1",
+           "options":[
+               {"describe":null, "items":null}
+           ]
+       },
+       {
+           "title":"对象最突出的特征是:",
+           "name":"1_2",
+           "options":[
+               {"describe":null,  "items":null},
+               {"describe":null,  "items":null},
+               {"describe":null,  "items":null}
+           ]
+       }
+     ]
+});
 
 };
 
