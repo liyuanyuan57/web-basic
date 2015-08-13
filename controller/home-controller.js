8851302520e047880f6ac0ff085e2c874492703a
liyuanyuan57 <liyuanyuan0507@outlook.com>
2015-08-13 14:50:30 +0800

    simulate data to test to generate the page of simple dynamically

--- controller/home-controller.js
+++ controller/home-controller.js
@@ -21,7 +21,30 @@ res.render('index',{
                {"describe":null,  "items":null}
            ]
        }
-     ]
+     ],
+
+    "simpleTopics":[
+        {
+            "title":"UML 与软件工程的关系是:",
+            "name":"2_1",
+            "options":[
+                {"describe":"(A)UML 就是软件工程", "items":"A"},
+                {"describe":"(B)UML 参与到软件工程中软件开发过程的几个阶段",  "items":"B"},
+                {"describe":"(C)UML 与软件工程无关",  "items":"C"},
+                {"describe":"(D)UML 是软件工程的一部分",  "items":"D"}
+            ]
+        },
+        {
+            "title":"Java 语言支持:",
+            "name":"2_2",
+            "options":[
+                {"describe":"(A)UML 就是软件工程", "items":"A"},
+                {"describe":"(B)UML 参与到软件工程中软件开发过程的几个阶段",  "items":"B"},
+                {"describe":"(C)UML 与软件工程无关",  "items":"C"},
+                {"describe":"(D)UML 是软件工程的一部分",  "items":"D"}
+            ]
+        }
+    ]
 });
 
 };
