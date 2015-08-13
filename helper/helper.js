354c341b6bc7fa101d23f7b7a790eb7f65f61888
liyuanyuan57 <liyuanyuan0507@outlook.com>
2015-08-13 17:17:41 +0800

    getTopics

--- helper/helper.js
+++ helper/helper.js
@@ -1,3 +1,19 @@
 /**
  * Created by liyuanyuan on 8/13/15.
  */
+var Factory = require('../model/factory.js');
+
+function  TopicHelper(){
+}
+
+TopicHelper.prototype.getTopics = function(data){
+    var topics = [];
+    var factory = new Factory();
+    data.topics.forEach(function(topic){
+        topics.push(factory.creatTopic(topic));
+    });
+
+    return topics;
+};
+
+module.exports = TopicHelper;
