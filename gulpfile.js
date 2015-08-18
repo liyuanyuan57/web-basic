8f479cb93d25368a153b3f3d9732e0dc4bdea165
liyuanyuan57 <liyuanyuan0507@outlook.com>
2015-08-19 00:21:08 +0800

    set gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 var gulp = require('gulp');
+var exec = require('child_process').exec;
+
+var paths = ['./controller/*','./helper/*','./view-models/*', './views/*','./views/partial/*'];
+
+gulp.task('creat', function() {
+  exec('mysql -u root -plenovo123 <./seeds/data.sql');
+});
+
+gulp.task('start', function() {
+  exec('node app.js');
+});
 
 gulp.task('default', function() {
-  console.log('hehe');
+  exec('node app.js');
+  gulp.watch(paths, ['start']);
 });
