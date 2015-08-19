5a8a5604666a39cf371f6f5e0a976c0df514dac5
liyuanyuan57 <liyuanyuan0507@outlook.com>
2015-08-19 10:18:45 +0800

    set server restarted

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,21 @@
 var gulp = require('gulp');
 var exec = require('child_process').exec;
+var nodemon = require('gulp-nodemon');
 
-var paths = ['./controller/*','./helper/*','./view-models/*', './views/*','./views/partial/*'];
+var paths = ['./controller/*', './helper/*', './view-models/*', './views/*', './views/partial/*'];
 
-gulp.task('creat', function() {
+gulp.task('dbinit', function() {
   exec('mysql -u root -plenovo123 <./seeds/data.sql');
+  console.log('database init success!');
 });
 
 gulp.task('start', function() {
-  exec('node app.js');
-});
-
-gulp.task('default', function() {
-  exec('node app.js');
-  gulp.watch(paths, ['start']);
-});
+  nodemon({
+      script: 'app.js',
+      ext: 'js json',
+      ignore: ['public/*']
+    })
+    .on('restart', function() {
+      console.log('restarted!')
+    })
+})
